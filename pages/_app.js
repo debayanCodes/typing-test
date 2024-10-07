a0d73f60d595d8e1ef8170b4f66b579ab7ee94ca
debayanCodes <downloadingmedia@gmail.com>
2024-10-07 23:01:10 +0530

    updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+// pages/_app.js
+import Head from 'next/head';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Typing Test</title>
+        <meta name="description" content="Your favourite app for testing your typing speed" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
